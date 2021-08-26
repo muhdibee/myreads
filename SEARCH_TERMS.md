dfbc7cc629fbe6007d7d4264e7d15d9437a9818d
Muhammad <muhdibee@gmail.com>
2021-08-26 03:58:10 +0100

    Implemented routing across pages and Implemented the Books shelf  of the home page.

--- SEARCH_TERMS.md
+++ SEARCH_TERMS.md
@@ -1,1 +1,28 @@
 'Android', 'Art', 'Artificial Intelligence', 'Astronomy', 'Austen', 'Baseball', 'Basketball', 'Bhagat', 'Biography', 'Brief', 'Business', 'Camus', 'Cervantes', 'Christie', 'Classics', 'Comics', 'Cook', 'Cricket', 'Cycling', 'Desai', 'Design', 'Development', 'Digital Marketing', 'Drama', 'Drawing', 'Dumas', 'Education', 'Everything', 'Fantasy', 'Film', 'Finance', 'First', 'Fitness', 'Football', 'Future', 'Games', 'Gandhi', 'Homer', 'Horror', 'Hugo', 'Ibsen', 'Journey', 'Kafka', 'King', 'Lahiri', 'Larsson', 'Learn', 'Literary Fiction', 'Make', 'Manage', 'Marquez', 'Money', 'Mystery', 'Negotiate', 'Painting', 'Philosophy', 'Photography', 'Poetry', 'Production', 'Programming', 'React', 'Redux', 'River', 'Robotics', 'Rowling', 'Satire', 'Science Fiction', 'Shakespeare', 'Singh', 'Swimming', 'Tale', 'Thrun', 'Time', 'Tolstoy', 'Travel', 'Ultimate', 'Virtual Reality', 'Web Development', 'iOS'
+
+
+
+allowAnonLogging: true
+authors: ["William E. Shotts, Jr."]
+averageRating: 4
+canonicalVolumeLink: "https://market.android.com/details?id=book-nggnmAEACAAJ"
+categories: ["COMPUTERS"]
+contentVersion: "1.2.2.0.preview.2"
+description: "You've experienced the shiny, point-and-click surface of your Linux computer—now dive below and explore its depths with the power of the command line. The Linux Command Line takes you from your very first terminal keystrokes to writing full programs in Bash, the most popular Linux shell. "
+id: "nggnmAEACAAJ"
+imageLinks: {smallThumbnail: "http://books.google.com/books/content?id=nggnmAEAC…J&printsec=frontcover&img=1&zoom=5&source=gbs_api", thumbnail: "http://books.google.com/books/content?id=nggnmAEAC…J&printsec=frontcover&img=1&zoom=1&source=gbs_api"}
+industryIdentifiers: (2) [{…}, {…}]
+infoLink: "https://play.google.com/store/books/details?id=nggnmAEACAAJ&source=gbs_api"
+language: "en"
+maturityRating: "NOT_MATURE"
+pageCount: 480
+panelizationSummary: {containsEpubBubbles: false, containsImageBubbles: false}
+previewLink: "http://books.google.com/books?id=nggnmAEACAAJ&dq=linux&hl=&cd=3&source=gbs_api"
+printType: "BOOK"
+publishedDate: "2012"
+publisher: "No Starch Press"
+ratingsCount: 2
+readingModes: {text: true, image: false}
+shelf: "currentlyReading"
+subtitle: "A Complete Introduction"
+title: "The Linux Command Line"
---
+++ src/HomePage.js
@@ -0,0 +1,138 @@
+import React, {} from 'react';
+import {Link} from 'react-router-dom';
+
+ const Home = ({inShelvebooks}) => {
+   const currentRead = [];
+   const wantToRead = [];
+   const read = [];
+
+   const findCurrentRead = (book)=>{
+     if(book.shelf === 'currentlyReading')
+      return currentRead.push(book);
+   }
+
+   const findWantToRead = (book)=>{
+    if(book.shelf === 'wantToRead') return wantToRead.push(book)
+  }
+
+  const findRead = (book)=>{
+    if(book.shelf === 'read') return read.push(book)
+  }
+
+     inShelvebooks.map((book) => {
+        findCurrentRead(book);
+        findWantToRead(book);
+        findRead(book)
+        return('')
+     })
+
+      return (
+            <div className="list-books">
+              <div className="list-books-title">
+                <h1>MyReads</h1>
+              </div>
+              <div className="list-books-content">
+                <div>
+                  <div className="bookshelf">
+                    <h2 className="bookshelf-title currently-reading">Currently Reading</h2>
+                    <div className="bookshelf-books">
+                      <ol className="books-grid">
+                            {currentRead.map((book) => {
+                                return(
+                                  <li key={book.id}>
+                                    <div className="book">
+                                        <div className="book-top">
+                                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                                        <div className="book-shelf-changer">
+                                            <select>
+                                                <option value="move" disabled>Move to...</option>
+                                                <option value="currentlyReading">Currently Reading</option>
+                                                <option value="wantToRead">Want to Read</option>
+                                                <option value="read">Read</option>
+                                                <option value="none">None</option>
+                                            </select>
+                                        </div>
+                                        </div>
+                                        <div className="book-title">{book.title}</div>
+                                        <div className="book-authors">{book.authors}</div>
+                                    </div>
+                                  </li>
+                                )
+                            }
+                             )
+                            }
+                      </ol>
+                    </div>
+                  </div>
+                  <div className="bookshelf">
+                    <h2 className="bookshelf-title want-to-read">Want to Read</h2>
+                    <div className="bookshelf-books">
+                    <ol className="books-grid">
+                            {wantToRead.map((book) => {
+                                return(
+                                  <li key={book.id}>
+                                    <div className="book">
+                                        <div className="book-top">
+                                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                                        <div className="book-shelf-changer">
+                                            <select>
+                                                <option value="move" disabled>Move to...</option>
+                                                <option value="currentlyReading">Currently Reading</option>
+                                                <option value="wantToRead">Want to Read</option>
+                                                <option value="read">Read</option>
+                                                <option value="none">None</option>
+                                            </select>
+                                        </div>
+                                        </div>
+                                        <div className="book-title">{book.title}</div>
+                                        <div className="book-authors">{book.authors}</div>
+                                    </div>
+                                  </li>
+                                )
+                            }
+                             )
+                            }
+                      </ol>
+                    </div>
+                  </div>
+                  <div className="bookshelf">
+                    <h2 className="bookshelf-title read">Read</h2>
+                    <div className="bookshelf-books">
+                    <ol className="books-grid">
+                            {read.map((book) => {
+                                return(
+                                  <li key={book.id}>
+                                    <div className="book">
+                                        <div className="book-top">
+                                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                                        <div className="book-shelf-changer">
+                                            <select>
+                                                <option value="move" disabled>Move to...</option>
+                                                <option value="currentlyReading">Currently Reading</option>
+                                                <option value="wantToRead">Want to Read</option>
+                                                <option value="read">Read</option>
+                                                <option value="none">None</option>
+                                            </select>
+                                        </div>
+                                        </div>
+                                        <div className="book-title">{book.title}</div>
+                                        <div className="book-authors">{book.authors}</div>
+                                    </div>
+                                  </li>
+                                )
+                            }
+                             )
+                            }
+                      </ol>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="open-search">
+                  <Link to='/search'><button>Add a book</button></Link>
+              </div>
+            </div>
+      )
+}
+
+  export default Home;

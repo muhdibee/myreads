c3e07e258ededcfe52682dbb0c9f2dcca767768a
Muhammad <muhdibee@gmail.com>
2021-12-13 16:22:40 +0100

    Created build folder

---
+++ build/precache-manifest.c697d3f484e9b993626d5c1e0d548b6a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "50d0e296594a98317ac6",
+    "url": "/static/css/main.abc70069.chunk.css"
+  },
+  {
+    "revision": "50d0e296594a98317ac6",
+    "url": "/static/js/main.50d0e296.chunk.js"
+  },
+  {
+    "revision": "c9168fbd136f034a4643",
+    "url": "/static/js/1.c9168fbd.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "e075fcfd2b4d2659e21f968caea9c38e",
+    "url": "/index.html"
+  }
+];
